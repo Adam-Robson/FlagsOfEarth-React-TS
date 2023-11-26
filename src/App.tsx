c1d8db20206e4e0ff09d204bc782577fb5521c8f
Adam Robson <adamray312@gmail.com>
2023-11-25 16:58:58 -0800

    add services folder and supabase and fetch all

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Countries from './components/Countries';
+
 export default function App() {
   return (
     <>
       <div>
         <h1>Vite + TypeScript + React</h1>
+        <Countries />
       </div>
     </>
   )
